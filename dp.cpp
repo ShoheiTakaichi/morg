62f936b144fce9587a79913a095980b24e025be0
takaichi <shohei.takaichi@gmail.com>
2018-06-02 19:37:57 +0900

    renew

--- dp.cpp
+++ dp.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -22,31 +22,80 @@ std::string itocom (int st,int n,int buy,int sell){
   return ans;
 }
 
-std::iTostock(int i){
+std::string iTostock(int i){
   std::string stock = "";;
-  if (st==0) stock = "A,";
-  if (st==1) stock = "B,";
-  if (st==2) stock = "C,";
+  if (i==0) stock = "A,";
+  if (i==1) stock = "B,";
+  if (i==2) stock = "C,";
   return stock;
+}
+
+struct answer{
+  int value;
+  int total;
+  std::vector<int> stock;
+  std::vector<int> buyT;
+  std::string command;
+}
+
+int stock_value(std::vector<int>stock,int T){
+  int ans=0;
+  ans += stock[0]*data[0][T];
+  ans += stock[1]*data[1][T];
+  ans += stock[2]*data[2][T];
+  return ans;
 }
 
 
 
-struct answer{
-  int value;
-  int stock [3];
-  int buyT [3];
-  std::string command;
-};//解とそれを実現する命令
+//Tをみこしたt~Tでの選択(各銘柄最大一回）
+//buy keep sell のどれかを行う。各銘柄
+answer gready(answer anst,int t,int T){
+  answer ans;
+  ans.stock = std::vector<int>(3);
+  ans.buyT = std::vector<int>(3);
+  std::vector <double> dif (3);
+  for(int i=0;i<3;i++){
+    dif[i] = data[T][i]-data[t][i];
+    dif[i] /= data[t][i];//効率
+  }
+  //全キープを初期値
+  //int best_total = stock_value(anst.stock,T);
+  int argmax = std::distance(dif.begin(), std::max_element(dif.begin(), dif.end()));
+  int argmin = std::distance(dif.begin(), std::min_element(dif.begin(), dif.end()));
+  int mean = 0*(0!=argmax&&0!=argmin) + 2*(2!=argmax&&2!=argmin) + 2*(2!=argmax&&2!=argmin);
+
+  if (
+
+
+
+
+  return ans;
+}
+
+
+
 
 
 
 int main(){
   answer ans[320];
   ans[0].value=10000;
-  std::fill(ans[0].stock,ans[0].stock+3,0);
-  std::fill(ans[0].buyT,ans[0].buyT+3,0);
-  for (int T = 0;T<320;T++){
+  ans[0].total=10000;
+  for (int i = 0;i<320;i++){
+    ans[i].stock = std::vector<int>(3);
+    ans[i].buyT = std::vector<int>(3);
+    std::fill(ans[i].stock.begin(),ans[i].stock.end(),0);
+    std::fill(ans[i].buyT.begin(),ans[i].buyT.end(),0);
+  }
+  for (int T=1;T<320;T++){
+    ans[T].total=0;
+    for (int t=0;t<T;t++){
+      answer temp = gready(ans[t],t,T);
+      if (temp.total>ans[T].total){
+        ans[T] = temp;
+      }
+    }
   }
   std::cout << ans[319].command << std::endl;
 }
